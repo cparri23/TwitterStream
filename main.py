b55e9ffe3abb290a71b4ba3f7cd2383cc2d20b1c
cparri23 <cparri23@users.noreply.github.com>
2020-05-15 10:53:37 -0400

    Writes to .json instead of just printing

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+class TwitterStreamer():
+    # Class for streaming tweets
+    def stream_tweets(self, fetchedTweetsOutputFile, keywords):
+        listener = StdOutListener(fetchedTweetsOutputFile)
+        auth = OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
+        auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+
+        stream = Stream(auth, listener)
+
+        stream.filter(track=keywords)
+
 class StdOutListener(StreamListener):
+        def __init__(self, fetchedTweetsOutputFile):
+            self.fetchedTweetsOutputFile = fetchedTweetsOutputFile
         def on_data(self, data):
-            print(data)
+            try:
+                print(data)
+                with open(self.fetchedTweetsOutputFile, 'a') as tf:
+                    tf.write(data)
+            except BaseException as e:
+                print("Error on data: %s" % str(e))
             return True
 
         def on_error(self, status):
@@ -14,10 +32,8 @@ class StdOutListener(StreamListener):
             return
 
 if __name__ == "__main__":
-    listener = StdOutListener()
-    auth = OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
-    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+    keyWords = [" rat "]
+    outputFile = "tweets.json"
 
-    stream = Stream(auth, listener)
-
-    stream.filter(track=['rat'])
+    streamer = TwitterStreamer()
+    streamer.stream_tweets(outputFile, keyWords)
